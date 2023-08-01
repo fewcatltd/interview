69ee4cfbe1c0c8811732f9d57b78c4fb80ae938c
fewcatltd <137312580+fewcatltd@users.noreply.github.com>
2023-08-01 09:30:53 +0400

    Create sum.js

---
+++ sum.js
@@ -0,0 +1,13 @@
+/*
+  Implement function sum:
+  sum(1)(2)() => 3
+  sum(1)() => 1
+*/
+
+function sum() {
+}
+
+function test() {
+  console.log(sum(1)(2)(3)(), 'Answer is 6') 
+  console.log(sum(9)(), 'Answer is 9')
+}
